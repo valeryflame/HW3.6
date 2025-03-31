ed48fa453ecb11b382d1c32a229517496b48771f
valeowine <aeryflame@gmail.com>
2025-03-31 18:55:27 +0300

    fix

--- src/test/java/lesson6/tests/PracticeFormTest.java
+++ src/test/java/lesson6/tests/PracticeFormTest.java
@@ -37,7 +37,6 @@ public class PracticeFormTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
     }
 
     @Test
